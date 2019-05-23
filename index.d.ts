583775dd57014ee00d1bc718954cc286b28a9e21
Damilola Rafael Jegede <dami.jegede@gmail.com>
2019-05-23 07:40:02 +0100

    Added TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function listen (port: number): void;
+export function get (route: string, callback: Function): void;
+export function post (route: string, callback: Function): void;
+export function setHeader (attribute: string, value: string): void;
